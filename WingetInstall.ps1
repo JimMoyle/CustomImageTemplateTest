db601a476060d4123042b3b6e9ad81a57dfc12c2
JimMoyle <github@jimmoyle.com>
2023-12-10 17:31:59 +0000

    now inget upgrade

--- WingetInstall.ps1
+++ WingetInstall.ps1
@@ -3,4 +3,4 @@ $outFile = (Join-Path $env:Temp Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msix
 Invoke-WebRequest -Uri aka.ms/getwinget -OutFile $outFile
 Add-AppPackage $outFile
 
-Winget Upgrade Winget --accept-source-agreements --accept-package-agreements
+#Winget Upgrade Winget --accept-source-agreements --accept-package-agreements
